cb2880228ba4d36ff4cc8d3f8a5857e5b9a9170d
CypherousSkies <5472563+CypherousSkies@users.noreply.github.com>
2021-10-04 18:06:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     name='reading4listeners',
     version='0.0.1',
     packages=['r4l'],
-    url='https://github.com/CypherousSkies/pdf-to-speech',
+    url='https://github.com/CypherousSkies/reading-for-listeners',
     license=legal,
     author='CypherousSkies',
     author_email="5472563+CypherousSkies@users.noreply.github.com",
     description='A deep-learning powered application which turns pdfs into audio files. Featuring ocr improvement and tts with inflection!',
     long_description=readme,
     install_requires=reqs,
-    entry_points={"console_scripts": ["p2s = r4l.bin.cli:main"], }
+    entry_points={"console_scripts": ["r4l = r4l.bin.cli:main"], }
 )
