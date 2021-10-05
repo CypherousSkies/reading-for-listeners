e9bce257ff92740bded446a4608842daaf825673
CypherousSkies <5472563+CypherousSkies@users.noreply.github.com>
2021-10-04 20:03:14 -0400

    simplified setup to make pypi play nicer

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ setup(
     version='0.0.2',
     packages=['r4l'],
     url='https://github.com/CypherousSkies/reading-for-listeners',
-    license=legal,
+    #license=legal,
+    license='AGPL-3',
     author='CypherousSkies',
     author_email="5472563+CypherousSkies@users.noreply.github.com",
     description='A deep-learning powered application which turns pdfs into audio files. Featuring ocr improvement and tts with inflection!',
-    long_description=readme,
+    #long_description=readme,
     install_requires=reqs,
     entry_points={"console_scripts": ["r4l = r4l.bin.cli:main"], }
 )
